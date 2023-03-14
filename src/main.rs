5b50750a7c007ecbca8d1cc28b5db924cec5d238
Jon Kovach <jlkovach01@gmail.com>
2023-03-14 15:54:16 -0400

    removed controller crate

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
-mod controller;
 mod finance;
 mod models;
-use axum::{routing::get, Router};
+use axum::{extract::Query, routing::get, Json, Router};
+use models::{BasicLiquidityRatio, BreakEvenPoint, CompoundInterest};
+
+use crate::finance::Calculation;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route(
-            "/basicliquidityratio",
-            get(controller::get_basic_liquidity_ratio),
-        )
-        .route("/breakevenpoint", get(controller::get_break_even_point))
-        .route("/compoundinterest", get(controller::get_compound_interest));
+        .route("/basicliquidityratio",get(create_response::<BasicLiquidityRatio>))
+        .route("/breakevenpoint", get(create_response::<BreakEvenPoint>))
+        .route("/compoundinterest",get(create_response::<CompoundInterest>));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+pub async fn create_response<T: Calculation>(query: Query<T>) -> Json<T> {
+    let mut response = query.0;
+    response.calculate();
+    Json(response)
 }
