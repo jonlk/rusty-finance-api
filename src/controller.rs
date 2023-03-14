e82af23c95aa675a1fe563cd4aafd968b9185d47
Jon Kovach <jlkovach01@gmail.com>
2023-03-14 05:02:22 -0400

    added break even point

--- src/controller.rs
+++ src/controller.rs
@@ -1,11 +1,26 @@
 use axum::{extract::Query, Json};
 
-use crate::{finance::Calculation, models::BasicLiquidityRatio};
+use crate::{
+    finance::Calculation,
+    models::{BasicLiquidityRatio, BreakEvenPoint},
+};
 
 pub async fn get_basic_liquidity_ratio(
-    blr: Query<BasicLiquidityRatio>,
+    query: Query<BasicLiquidityRatio>,
 ) -> Json<BasicLiquidityRatio> {
-    let mut test: BasicLiquidityRatio = blr.0;
-    test.calculate();
-    Json(test)
+    let mut model = query.0;
+    model.calculate();
+    Json(model)
 }
+
+pub async fn get_break_even_point(query: Query<BreakEvenPoint>) -> Json<BreakEvenPoint> {
+    let mut model = query.0;
+    model.calculate();
+    Json(model)
+}
+
+//TODO: Figure out how to route all responses to one handler
+// pub async fn create_response<T: Calculation>(model: &mut T) -> Json<&mut T> {
+//     model.calculate();
+//     Json(model)
+// }
